9d27f2116a6bd38cba15eea265b4ffca4564394c
Alissonc79 <89618940+Alissonc79@users.noreply.github.com>
2021-10-28 22:26:14 -0300

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+    $usuario = $_SESSION["user"];
+    if(!isset($usuario)){
+        header('location:index.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="content-language" content="pt-br"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Boutique Store</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <header>
+            <img src="imagens/logo.png" width="20%" height="20%"/>
+            <nav class="navbar navbar-expand-lg navbar-ligth bg-ligth">
+                <?php
+                    include 'menu.php';
+                ?>
+                <div class="container-fluid">
+                    <h3><b> </b></h3>
+                    <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">Inserir Usuário</button>
+                </div>
+            </nav>
+        </header>
+        <div class="container-fluid">
+            <section>
+                <br/>
+                <div class="row justify-content-center row-cols-2 row-cols-md-1 mb-3 text-center">
+                    <div class="col">
+                        <div class="card mb-4 rounded-3 shadow-sm">
+                            <div class="card-hearder py-3">
+                            <h4 class="my-0 fw-normal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
+  <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+  <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
+</svg><i class="bi bi-person-workspace"></i>&nbsp;<b>Usuários</b></h4>
+                            </div>
+                            <div class="card-body">
+                                <?php
+                                    include 'listagemusuario.php';
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg>&nbsp;<h5 class="modal-title" id="exampleModalLabel">Inserir Usuário</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <?php
+                            include 'cadastrousuario.php';
+                        ?>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <hr/>
+            <center><b>Acesso a Banco de Dados - Desenvolvida pela turma TDS06 - 2021</b></center>
+        </footer>
+    </body>
+</html>
